51c235f9f0572985a45b8c69599c4f7bd5ff23e6
ichmen <ichmen@rambler.ru>
2022-06-28 19:01:02 +0300

    squared fixed

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,3 @@
-export const getSquareArray = arr => arr.map(el => el * el);
+export const getSquaredArray = arr => arr.map(el => el * el);
 export const getOddNumbers = arr => arr.filter(el => el % 2);
 export default (a, b) => a + b;
