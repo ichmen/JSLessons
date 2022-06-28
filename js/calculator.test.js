c58aedc19ee768d7f411762fee52f6616dc522eb
ichmen <ichmen@rambler.ru>
2022-06-28 19:01:34 +0300

    all fixed

--- js/calculator.test.js
+++ js/calculator.test.js
@@ -1,10 +1,10 @@
-import sum, { getSquareArray, getOddNumbers } from './calculator.js';
+import sum, { getSquaredArray, getOddNumbers } from './calculator.js';
 it('shuld return sum', () => {
   const result = sum(2, 3);
   expect(result).toEqual(5);
 });
 it('shuld return squares', () => {
-  const result = getSquareArray([1, 2, 3]);
+  const result = getSquaredArray([1, 2, 3]);
   expect(result).toEqual([1, 4, 9]);
 });
 it('should return odds from list', () => {
