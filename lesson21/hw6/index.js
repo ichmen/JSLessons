c628f91691d6093bd82cf69ac56e49609a006a38
ichmen <ichmen@rambler.ru>
2022-07-06 18:42:16 +0300

    hw6

---
+++ lesson21/hw6/index.js
@@ -0,0 +1,15 @@
+export function clearList() {
+  const searchElem = document.querySelector('.categories');
+  searchElem.innerHTML = '';
+  // searchElem.textContent = `<button>${buttonText}</button>`;
+  // console.dir(searchElem);
+  // searchElem.textContent = String(text);
+  // document.querySelector('.title').textContent = String(text);
+}
+// setButton('AAAAAA');
+
+// console.log(getTitle());
+// console.log(getDescription());
+// console.log(getPlans());
+// console.log(getGoal());
+// export { setTitle };
