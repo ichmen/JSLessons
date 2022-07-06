01f5c623f35991e34155671e03e7ed31ece20ef2
ichmen <ichmen@rambler.ru>
2022-07-06 18:15:59 +0300

    new

--- lesson21/hw4/index.js
+++ lesson21/hw4/index.js
@@ -1,10 +1,13 @@
-function setTitle(text) {
-  document.querySelector('.title').textContent = String(text);
+export function setTitle(text) {
+  const searchElem = document.querySelector('.title');
+  // console.log(searchElem);
+  searchElem.textContent = String(text);
+  // document.querySelector('.title').textContent = String(text);
 }
-setTitle('AAAAAA');
+// setTitle('AAAAAA');
 
 // console.log(getTitle());
 // console.log(getDescription());
 // console.log(getPlans());
 // console.log(getGoal());
-export { setTitle };
+// export { setTitle };
