04714f3cdcc1e0be6a7430c004abb05557b6eee9
ichmen <ichmen@rambler.ru>
2022-07-13 17:05:45 +0300

    export added

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,4 +1,4 @@
-const addImage = (imgSrc, callback) => {
+export const addImage = (imgSrc, callback) => {
   const container = document.querySelector('.page');
   const image = document.createElement('img');
   image.setAttribute('src', imgSrc);
