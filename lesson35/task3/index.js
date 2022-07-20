87243b132e3ff6fb2112b1eec47664713ce4b355
ichmen <ichmen@rambler.ru>
2022-07-20 19:54:27 +0300

    new

---
+++ lesson35/task3/index.js
@@ -0,0 +1,32 @@
+import { request, requestRepos } from './gateWay.js';
+import { renderData, renderRepos } from './render.js';
+
+const inputElem = document.querySelector('.name-form__input');
+const buttonElem = document.querySelector('.name-form__btn');
+const spinner = document.querySelector('.spinner');
+const baseUrl = 'https://api.github.com/users/';
+const defaultUser = {
+  name: '',
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  location: '',
+};
+renderData(defaultUser);
+buttonElem.addEventListener('click', buttonHandler);
+
+function buttonHandler() {
+  request(baseUrl + inputElem.value)
+    .then(data => {
+      spinner.classList.remove('spinner_hidden');
+      renderData(data);
+      return data.repos_url;
+    })
+    .then(url => requestRepos(url))
+    .then(data => {
+      renderRepos(data);
+      spinner.classList.add('spinner_hidden');
+    })
+    .catch(() => {
+      spinner.classList.add('spinner_hidden');
+      alert('Failed to load data');
+    });
+}
